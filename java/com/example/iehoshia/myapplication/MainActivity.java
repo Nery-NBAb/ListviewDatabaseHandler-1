a288cc3f0894f327cad6b9e57d1901457bab4400
iehoshia <jepgez@gmail.com>
2016-05-16 11:44:45 -0600

    Actualización para notificaciones y conexión a internet
    Actualización para notificaciones y conexión a internet

--- java/com/example/iehoshia/myapplication/MainActivity.java
+++ java/com/example/iehoshia/myapplication/MainActivity.java
@@ -1,13 +1,61 @@
 package com.example.iehoshia.myapplication;
 
+import android.app.Activity;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static final int NOTIFICATION_ID = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        TextView estaConectado = (TextView) findViewById(R.id.textView);
+        if (estaConectado())
+            estaConectado.setText("Esta Conectado");
+        else
+            estaConectado.setText("No Esta Conectado");
+        mostrarNotificacion();
+
+    }
+
+    public void mostrarNotificacion(){
+        Intent intent = new Intent(Intent.ACTION_VIEW,
+                Uri.parse("www.google.com"));
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, 0);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this);
+
+        builder.setSmallIcon(R.mipmap.ic_launcher);
+        builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher));
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(true);
+        builder.setContentTitle("Primera Notificacion");
+        builder.setContentText("ES la primera notificación");
+        builder.setSubText("Favor atender. Urgente. ");
+
+        NotificationManager notificationManager = (NotificationManager) getSystemService(
+                NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    private boolean estaConectado() {
+        ConnectivityManager connManager = (ConnectivityManager)getSystemService(Activity.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connManager.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected())
+            return true;
+        else
+            return false;
+
     }
 }
